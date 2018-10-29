f51a70e05dfe1b22f7fe48e5d201817314fe3485
AntonSynytsia <anton.synytsia@gmail.com>
2018-10-28 20:38:25 -0700

    Fixed compile errors in CLOOK and modifed Kconfig for inclusion

--- linux-yocto-3.19/block/clook-iosched.c
+++ linux-yocto-3.19/block/clook-iosched.c
@@ -111,16 +111,18 @@ static void clook_add_request(struct request_queue *q, struct request *rq)
 
     // In this case, we achieve a sorted doubly linked list (takes O(n) but does the job)
 
-    struct list_head* head = &nd->queue;
-    struct list_head* item = &rq->queuelist;
+    struct clook_data *nd = q->elevator->elevator_data;
+
+    struct list_head *head = &nd->queue;
+    struct list_head *item = &rq->queuelist;
 
     // Find node to append to
-    struct list_head* node = head;
-    struct list_head* next_node = head->next;
+    struct list_head *node = head;
+    struct list_head *next_node = head->next;
 
     while (next_node != head) { // while node is not last
         // get request at next node
-        struct nrq = list_entry(next_node, struct request, queuelist);
+        struct request *nrq = list_entry(next_node, struct request, queuelist);
 
         // if request at next node is greater than rq, break out
         if (blk_rq_pos(nrq) > blk_rq_pos(rq)) break;
@@ -216,4 +218,4 @@ module_exit(clook_exit);
 
 MODULE_AUTHOR("Jens Axboe");
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("C-Look IO scheduler");
+MODULE_DESCRIPTION("CLOOK IO scheduler");
