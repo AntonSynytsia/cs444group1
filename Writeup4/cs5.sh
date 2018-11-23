d7f69a5be4c5b4aaf85923c11580c2949762ce8c
AntonSynytsia <anton.synytsia@gmail.com>
2018-11-22 22:49:31 -0800

    Added options to control repeat or one-off modes

--- Writeup4/cs5.sh
+++ Writeup4/cs5.sh
@@ -1,20 +1,21 @@
+# After logging in, switch mode
 sudo su
-
 # optional: turn off lights
 echo none > /sys/class/leds/led0/trigger
 echo none > /sys/class/leds/led1/trigger
-
 # launch
 echo morse > /sys/class/leds/led1/trigger
-
 # getting speed
 cat /sys/class/leds/led1/speed
-
 # setting speed (clamped between 1 - 1000)
 echo 10 > /sys/class/leds/led1/speed
-
 # getting message
 cat /sys/class/leds/led1/message
-
 # setting message
 echo "Hello world" > /sys/class/leds/led1/message
+# getting mode
+cat /sys/class/leds/led1/mode
+# setting "one-off" mode
+echo 0 > /sys/class/leds/led1/mode
+# setting "repeat" mode
+echo 1 > /sys/class/leds/led1/mode
--- linux/drivers/leds/trigger/ledtrig-morse.c
+++ linux/drivers/leds/trigger/ledtrig-morse.c
@@ -23,18 +23,19 @@
 typedef struct
 {
     int status, dignity, ego;
-} dummy_arg_t;
+} msg_arg_t;
 
-#define DUMMY_GET_VARIABLES _IOR('q', 1, dummy_arg_t *)
-#define DUMMY_CLR_VARIABLES _IO('q', 2)
-#define DUMMY_SET_VARIABLES _IOW('q', 3, dummy_arg_t *)
+#define MSG_GET_VARIABLES _IOR('q', 1, msg_arg_t *)
+#define MSG_CLR_VARIABLES _IO('q', 2)
+#define MSG_SET_VARIABLES _IOW('q', 3, msg_arg_t *)
 
 struct morse_trig_data {
     char* message;
     unsigned int indexL; // Letter index
     unsigned int indexM; // Letter part index
     unsigned int delayM; // Every letter part is followed a delay. This indicates whether to wait or proceed to the next part.
-    unsigned int speed;
+    unsigned int speed; // 1 - 10000
+    unsigned int mode; // 0 - one-off; 1 - repeat
     struct timer_list timer;
 };
 
@@ -90,7 +91,9 @@ const int PART_DELAY = 1;
 
 const int INVALID_CHAR_DELAY = 3;
 
-const int DEFAULT_SPEED = 50;
+const unsigned int DEFAULT_SPEED = 50;
+const unsigned int DEFAULT_MODE = 1;
+
 const char* DEFAULT_MESSAGE = "Linux Operating Systems Group 1";
 
 
@@ -135,23 +138,23 @@ static void store_message(struct morse_trig_data *morse_data, const char *buf, s
 
 static int my_open(struct inode *i, struct file *f)
 {
-    printk(KERN_ALERT "CS444 Dummy driver open\r\n");
+    printk(KERN_ALERT "CS444 MSG driver open\r\n");
     return 0;
 }
 static int my_close(struct inode *i, struct file *f)
 {
-    printk(KERN_ALERT "CS444 Dummy driver close\r\n");
+    printk(KERN_ALERT "CS444 MSG driver close\r\n");
     return 0;
 }
 
-static ssize_t dummy_read(struct file *file, char __user *buf, size_t size, loff_t *ppos)
+static ssize_t msg_read(struct file *file, char __user *buf, size_t size, loff_t *ppos)
 {
-    printk(KERN_ALERT "CS444 Dummy driver read\r\n");
-    snprintf(buf, size, "Hey there, I'm a dummy!\r\n");
+    printk(KERN_ALERT "CS444 MSG driver read\r\n");
+    snprintf(buf, size, "Hey there, I'm a msg!\r\n");
     return strlen(buf);
 }
 
-static ssize_t dummy_write(struct file *file, const char __user *buf, size_t size, loff_t *ppos)
+static ssize_t msg_write(struct file *file, const char __user *buf, size_t size, loff_t *ppos)
 {
     char lcl_buf[64];
 
@@ -162,69 +165,19 @@ static ssize_t dummy_write(struct file *file, const char __user *buf, size_t siz
             return -EACCES;
         }
 
-    printk(KERN_ALERT "CS444 Dummy driver write %ld bytes: %s\r\n", size, lcl_buf);
+    printk(KERN_ALERT "CS444 MSG driver write %ld bytes: %s\r\n", size, lcl_buf);
 
     return size;
 }
 
-static struct file_operations dummy_fops =
+static struct file_operations msg_fops =
 {
     .owner = THIS_MODULE,
     .open = my_open,
-    .read = dummy_read,
-    .write = dummy_write,
+    .read = msg_read,
+    .write = msg_write,
     .release = my_close
 };
-
-static int __init dummy_init(void)
-{
-    int ret;
-    struct device *dev_ret;
-
-    // Allocate the device
-    if ((ret = alloc_chrdev_region(&dev, FIRST_MINOR, MINOR_CNT, "cs444_dummy")) < 0)
-    {
-        return ret;
-    }
-
-    cdev_init(&c_dev, &dummy_fops);
-
-    if ((ret = cdev_add(&c_dev, dev, MINOR_CNT)) < 0)
-    {
-        return ret;
-    }
-
-    // Allocate the /dev device (/dev/cs444_dummy)
-    if (IS_ERR(cl = class_create(THIS_MODULE, "char")))
-    {
-        cdev_del(&c_dev);
-        unregister_chrdev_region(dev, MINOR_CNT);
-        return PTR_ERR(cl);
-    }
-    if (IS_ERR(dev_ret = device_create(cl, NULL, dev, NULL, "cs444_dummy")))
-    {
-        class_destroy(cl);
-        cdev_del(&c_dev);
-        unregister_chrdev_region(dev, MINOR_CNT);
-        return PTR_ERR(dev_ret);
-    }
-
-    printk(KERN_ALERT "CS444 Dummy Driver has been loaded!\r\n");
-
-    return 0;
-}
-
-static void __exit dummy_exit(void)
-{
-    device_destroy(cl, dev);
-    class_destroy(cl);
-    cdev_del(&c_dev);
-    unregister_chrdev_region(dev, MINOR_CNT);
-}
-
-//module_init(dummy_init);
-//module_exit(dummy_exit);
-
 
 /*
  -------------------------------------------------------------------------------
@@ -258,7 +211,15 @@ static void led_morse_function(unsigned long data)
     }
 
     if (letter == '\0' || letter == '\n') {
-        // If finished processing last letter of the message, set delay to word delay and restart
+        // If finished processing last letter of the message, either:
+
+        // deactivate
+        if (morse_data->mode == 0) {
+            morse_trig_deactivate(led_cdev);
+            return;
+        }
+
+        // set delay to word delay and restart
         delay = MESSAGE_DELAY;
         morse_data->indexL = 0;
         morse_data->indexM = 0;
@@ -373,8 +334,42 @@ static ssize_t led_message_store(struct device *dev,
     return size;
 }
 
+static ssize_t led_mode_show(struct device *dev,
+        struct device_attribute *attr, char *buf)
+{
+    struct led_classdev *led_cdev = dev_get_drvdata(dev);
+    struct morse_trig_data *morse_data = led_cdev->trigger_data;
+
+    if (morse_data->mode == 0) {
+        return sprintf(buf, "%d: one-off\n", morse_data->mode);
+    }
+    else {
+        return sprintf(buf, "%d: repeat\n", morse_data->mode);
+    }
+}
+
+static ssize_t led_mode_store(struct device *dev,
+        struct device_attribute *attr, const char *buf, size_t size)
+{
+    struct led_classdev *led_cdev = dev_get_drvdata(dev);
+    struct morse_trig_data *morse_data = led_cdev->trigger_data;
+    unsigned long s;
+    int ret;
+
+    ret = kstrtoul(buf, 0, &s);
+    if (ret) return ret;
+
+    if (s == 0)
+        morse_data->mode = 0;
+    else
+        morse_data->mode = 1;
+
+    return size;
+}
+
 static DEVICE_ATTR(speed, 0644, led_speed_show, led_speed_store);
 static DEVICE_ATTR(message, 0644, led_message_show, led_message_store);
+static DEVICE_ATTR(mode, 0644, led_mode_show, led_mode_store);
 
 static void morse_trig_activate(struct led_classdev *led_cdev)
 {
@@ -399,18 +394,26 @@ static void morse_trig_activate(struct led_classdev *led_cdev)
         return;
     }
 
+    rc = device_create_file(led_cdev->dev, &dev_attr_mode);
+    if (rc) {
+        kfree(led_cdev->trigger_data);
+        return;
+    }
+
     setup_timer(&morse_data->timer, led_morse_function, (unsigned long) led_cdev);
     morse_data->message = NULL;
     morse_data->indexL = 0;
     morse_data->indexM = 0;
     morse_data->delayM = 0;
     morse_data->speed = DEFAULT_SPEED;
+    morse_data->mode = DEFAULT_MODE;
     store_message(morse_data, DEFAULT_MESSAGE, 256);
 
-    printk(KERN_ALERT "Message: %s, Speed: %d\n", morse_data->message, morse_data->speed);
+    printk(KERN_ALERT "Message: %s, Speed: %d, Mode: %d\n", morse_data->message, morse_data->speed, morse_data->mode);
 
     if (!led_cdev->blink_brightness)
         led_cdev->blink_brightness = led_cdev->max_brightness;
+
     led_morse_function(morse_data->timer.data);
     set_bit(LED_BLINK_SW, &led_cdev->work_flags);
     led_cdev->activated = true;
@@ -422,33 +425,35 @@ static void morse_trig_deactivate(struct led_classdev *led_cdev)
 
     if (led_cdev->activated) {
         del_timer_sync(&morse_data->timer);
+
         device_remove_file(led_cdev->dev, &dev_attr_speed);
         device_remove_file(led_cdev->dev, &dev_attr_message);
+        device_remove_file(led_cdev->dev, &dev_attr_mode);
+
         if (morse_data->message) {
             kfree(morse_data->message);
             morse_data->message = NULL;
         }
         kfree(morse_data);
+
         clear_bit(LED_BLINK_SW, &led_cdev->work_flags);
         led_cdev->activated = false;
     }
 }
 
 static struct led_trigger morse_led_trigger = {
-    .name     = "morse",
-    .activate = morse_trig_activate,
+    .name       = "morse",
+    .activate   = morse_trig_activate,
     .deactivate = morse_trig_deactivate,
 };
 
-static int morse_reboot_notifier(struct notifier_block *nb,
-                     unsigned long code, void *unused)
+static int morse_reboot_notifier(struct notifier_block *nb, unsigned long code, void *unused)
 {
     led_trigger_unregister(&morse_led_trigger);
     return NOTIFY_DONE;
 }
 
-static int morse_panic_notifier(struct notifier_block *nb,
-                     unsigned long code, void *unused)
+static int morse_panic_notifier(struct notifier_block *nb, unsigned long code, void *unused)
 {
     panic_morses = 1;
     return NOTIFY_DONE;
@@ -464,23 +469,62 @@ static struct notifier_block morse_panic_nb = {
 
 static int __init morse_trig_init(void)
 {
+    int ret;
+    struct device *dev_ret;
+
+    // Register trigger
     int rc = led_trigger_register(&morse_led_trigger);
 
     if (!rc) {
-        atomic_notifier_chain_register(&panic_notifier_list,
-                           &morse_panic_nb);
+        atomic_notifier_chain_register(&panic_notifier_list, &morse_panic_nb);
         register_reboot_notifier(&morse_reboot_nb);
     }
+
+    // Allocate the device
+    if ((ret = alloc_chrdev_region(&dev, FIRST_MINOR, MINOR_CNT, "msg")) < 0)
+    {
+        return ret;
+    }
+
+    cdev_init(&c_dev, &msg_fops);
+
+    if ((ret = cdev_add(&c_dev, dev, MINOR_CNT)) < 0)
+    {
+        return ret;
+    }
+
+    // Allocate the /dev device (/dev/msg)
+    if (IS_ERR(cl = class_create(THIS_MODULE, "char")))
+    {
+        cdev_del(&c_dev);
+        unregister_chrdev_region(dev, MINOR_CNT);
+        return PTR_ERR(cl);
+    }
+    if (IS_ERR(dev_ret = device_create(cl, NULL, dev, NULL, "msg")))
+    {
+        class_destroy(cl);
+        cdev_del(&c_dev);
+        unregister_chrdev_region(dev, MINOR_CNT);
+        return PTR_ERR(dev_ret);
+    }
+
+    printk(KERN_ALERT "CS444 MSG Driver has been loaded!\r\n");
 
     return rc;
 }
 
 static void __exit morse_trig_exit(void)
 {
+    // Unregister trigger
     unregister_reboot_notifier(&morse_reboot_nb);
-    atomic_notifier_chain_unregister(&panic_notifier_list,
-                     &morse_panic_nb);
+    atomic_notifier_chain_unregister(&panic_notifier_list, &morse_panic_nb);
     led_trigger_unregister(&morse_led_trigger);
+
+    // Exit character device
+    device_destroy(cl, dev);
+    class_destroy(cl);
+    cdev_del(&c_dev);
+    unregister_chrdev_region(dev, MINOR_CNT);
 }
 
 module_init(morse_trig_init);
